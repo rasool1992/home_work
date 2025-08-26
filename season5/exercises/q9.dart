55a78ca901d4222a92d51bae63f88f8b10d6bbff
rasool1992 <somanxx@yahoo.com>
2025-08-26 23:28:27 +0300

    season5

--- season5/exercises/q9.dart
+++ season5/exercises/q9.dart
@@ -2,9 +2,19 @@
 // Create a program with a page path stored in a variable. If the path is '/', print Home. For any other value, print 404.
 
 void main() {
-  String pagePath = '/';
-  if (pagePath == '/')
+  // * I will make to solution because you don't specify if you mean string data type or it map with key type string
+
+  // * if it's key type string
+  Map<String, String> direction = {'pagePath': '/home'};
+  if (direction['pagePath'] == '/')
     print('Home');
   else
     print('404');
+
+  // * if it is String data type
+  // String pagePath = '/';
+  // if (pagePath == '/')
+  //   print('Home');
+  // else
+  //   print('404');
 }
