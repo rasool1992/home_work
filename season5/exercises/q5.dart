068c6f9fb71d0ed6dc2e31c5c4e38d16fca1728a
rasool1992 <somanxx@yahoo.com>
2025-08-27 00:35:25 +0300

    season5

--- season5/exercises/q5.dart
+++ season5/exercises/q5.dart
@@ -4,7 +4,8 @@ void main() {
   String text = 'EGP 12.50';
   String newText = '';
   for (int i = 0; i < text.length; i++) {
-    if (text.codeUnitAt(i) >= 46 && text.codeUnitAt(i) <= 57) {
+    if ((text.codeUnitAt(i) >= 48 && text.codeUnitAt(i) <= 57) ||
+        text.codeUnitAt(i) == 46) {
       newText = newText + text[i];
     }
   }
